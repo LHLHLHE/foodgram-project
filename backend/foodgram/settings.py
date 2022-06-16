ecd1ce3288612fac4ac324ed055fbcd12b87805e
Lev Khalyapin <lev.halyapin@yandex.ru>
2022-06-16 15:22:34 +0300

    Updated debug switcher

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('SECRET_KEY', default=get_random_secret_key())
 
-DEBUG = os.getenv('DEBUG', default=False)
+DEBUG = (os.getenv('DEBUG', default=False) == 'True')
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='127.0.0.1').split()
 
